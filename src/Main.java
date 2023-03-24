e3879e110d4a57f81376858090507b36f8efbd45
Sakura <Sakura@126.com>
2023-03-24 11:15:49 +0800

    modify Main.java  commit

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("master");
         System.out.println("master");
         System.out.println("master111222");
+        System.out.println("function-game-test...");
     }
 }
